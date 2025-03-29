038b73809899f82ddf1390484146e51b3877fa48
Ajith PM <ajithpmdwaraka@gmail.com>
2025-03-29 18:04:30 +0530

    Added new feature Co-authored-by: The boysteam <theboysteam02@gmail.com>

---
+++ 08-Modules-and-Packages/3-importing_packages.py
@@ -0,0 +1,7 @@
+# Importing specific functions from a module
+
+from my_package.string_operations import to_uppercase, count_vowels
+
+text = "Hello World"
+print("Uppercase:", to_uppercase(text))
+print("Vowel count:", count_vowels(text))
