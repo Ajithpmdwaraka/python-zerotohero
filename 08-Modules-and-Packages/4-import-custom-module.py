9d48cb1b8fd395e8819c4cdff604c638bfc10999
Ajith PM <ajithpmdwaraka@gmail.com>
2025-03-29 18:05:30 +0530

    Added new feature Co-authored-by: The boysteam <theboysteam02@gmail.com>

---
+++ 08-Modules-and-Packages/4-import-custom-module.py
@@ -0,0 +1,8 @@
+# Importing a custom module
+
+from my_package import math_operations
+
+num1, num2 = 10, 5
+
+print("Addition:", math_operations.add(num1, num2))
+print("Subtraction:", math_operations.subtract(num1, num2))
